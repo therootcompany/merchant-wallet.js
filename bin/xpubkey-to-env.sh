b610a1143e694db40de7081c23c9fc881510059a
AJ ONeal <coolaj86@gmail.com>
2022-02-27 00:13:02 -0700

    fix(bin): test for .env before grep test for XPUB_KEY

--- bin/xpubkey-to-env.sh
+++ bin/xpubkey-to-env.sh
@@ -2,6 +2,9 @@
 set -e
 set -u
 
+if [[ ! -e .env ]]; then
+    touch .env
+fi
 if ! grep -q '^XPUB_KEY=' .env; then
     echo "XPUB_KEY=$(node ./bin/xpubkey.js)" >> .env
     echo "Saved Extended Public Key to .env (keep it secret for anonymity)"
