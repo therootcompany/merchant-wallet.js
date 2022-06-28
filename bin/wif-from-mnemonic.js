c97a6922c1d481dd38d166d3ef2c6518a11727a4
AJ ONeal <coolaj86@gmail.com>
2022-06-28 17:38:38 -0600

    WIP: display multiple QRs

--- bin/wif-from-mnemonic.js
+++ bin/wif-from-mnemonic.js
@@ -7,7 +7,9 @@ require("dotenv").config({ path: ".env.secret" });
 let Wallet = require("../lib/wallet.js").Wallet;
 
 async function main() {
-  let index = parseInt(process.argv[2], 10);
+  let indices = process.argv[2].split("-");
+  let index = parseInt(indices[0], 10);
+  let end = parseInt(indices[1], 10) || index;
   let coinName = process.argv[3] || process.env.WALLET_TYPE || "dash";
 
   if (!process.argv[2] || (!index && 0 !== index)) {
@@ -38,18 +40,66 @@ async function main() {
   }
 
   let coinNameUpper = myCoin.name.toUpperCase();
-  let wif = await derive.wifFromMnemonic(mnemonic, index, direction, account);
-  let ascii = await derive.wifQrFromMnemonic(mnemonic, index, {
-    format: "ascii",
-  }, direction, account);
-
   console.info();
   console.info(
     `    ====== [${coinNameUpper}] WIF (Wallet Import Format) Private Key ======    `
   );
   console.info();
-  console.info(ascii);
-  console.info(`    ${coinNameUpper}: ${wif}    `);
+
+  for (let i = index; i <= end; i += 2) {
+    let next = i + 1;
+    let wif1 = await derive.wifFromMnemonic(
+      mnemonic,
+      index + 0,
+      direction,
+      account
+    );
+    let ascii1 = await derive.wifQrFromMnemonic(
+      mnemonic,
+      index + 0,
+      { format: "ascii" },
+      direction,
+      account
+    );
+    //console.info(ascii1);
+
+    let wif2 = await derive.wifFromMnemonic(
+      mnemonic,
+      index + 1,
+      direction,
+      account
+    );
+    let ascii2 = await derive.wifQrFromMnemonic(
+      mnemonic,
+      index + 1,
+      { format: "ascii" },
+      direction,
+      account
+    );
+
+    let ascii1Lines = ascii1.split(/\n/);
+    let ascii2Lines = ascii2.split(/\n/);
+    let max = Math.max(ascii1Lines.length, ascii2Lines.length);
+    for (let n = 0; n < max; n += 1) {
+      let line1 = (ascii1Lines[n] || "").padStart(34, " ");
+      if (next <= end) {
+        let line2 = (ascii2Lines[n] || "").padStart(34, " ");
+        if (n === max - 1) {
+          // TODO why the offset?
+          console.info(`${line1}       ${line2}`);
+        } else {
+          console.info(`${line1}    ${line2}`);
+        }
+      } else {
+        console.info(`${line1}`);
+      }
+    }
+    if (i === end) {
+      console.info(`${coinNameUpper}: ${wif1}`);
+    } else {
+      console.info(`${coinNameUpper}: ${wif1}, ${wif2}`);
+    }
+  }
   console.info();
 }
 
